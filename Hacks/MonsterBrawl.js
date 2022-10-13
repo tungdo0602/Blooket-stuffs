d478e8d701ae3625094cdedbcd56161104862800
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-13 23:06:07 +0700

    Update MonsterBrawl.js

--- Hacks/MonsterBrawl.js
+++ Hacks/MonsterBrawl.js
@@ -1,1 +1,2 @@
 function setXP(amount){return getStateNode()[0]._owner.stateNode.setState({totalXp: amount, xp: amount})}
+function setLevel(amount){return getStateNode()[0]._owner.stateNode.setState({level: amount})}
