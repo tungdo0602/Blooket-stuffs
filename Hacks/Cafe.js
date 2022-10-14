4819cd6f601c7d0a9f91695da5d6da7d20af284e
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 20:30:17 +0700

    Create Cafe.js

---
+++ Hacks/Cafe.js
@@ -0,0 +1,7 @@
+function setCash(amount){return getStateNode().setState({cafeCash: amount})}
+function maxFood(){
+for(let i of Object.values(getStateNode().state.foods)){
+    i.level = 5
+    i.stock = Number.MAX_VALUE
+}
+}
