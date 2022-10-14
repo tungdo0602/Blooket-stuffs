670a9198725b7eba04829199853807aaaf54e19d
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 19:12:47 +0700

    Create towerDefense.js

---
+++ Hacks/towerDefense.js
@@ -0,0 +1,4 @@
+function setTokens(amount){return getStateNode().setState({tokens: amount})}
+function setDmg(){return getStateNode().setState({totalDmg: amount})}
+function setRound(){return getStateNode().setState({round: amount})}
+function setHealth(){return getStateNode().setState({health: amount})}
