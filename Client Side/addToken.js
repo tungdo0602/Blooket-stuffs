0eb17e5aff9222e7bfbe73b5e2e7d5ca03bb5295
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 18:49:27 +0700

    Update addToken.js

--- Client Side/addToken.js
+++ Client Side/addToken.js
@@ -1,2 +1,2 @@
 //https://dashboard.blooket.com/market
-let addToken = (amount) => getStateNode()[0]._owner.stateNode.setState({tokens: amount})
+let addToken = (amount) => getStateNode().setState({tokens: amount})
