80864a3026da0809bf2d3ab9adb6ce7db6c71e1a
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 19:13:34 +0700

    Update towerDefense.js

--- Hacks/towerDefense.js
+++ Hacks/towerDefense.js
@@ -1,4 +1,4 @@
 function setTokens(amount){return getStateNode().setState({tokens: amount})}
-function setDmg(){return getStateNode().setState({totalDmg: amount})}
-function setRound(){return getStateNode().setState({round: amount})}
-function setHealth(){return getStateNode().setState({health: amount})}
+function setDmg(amount){return getStateNode().setState({totalDmg: amount})}
+function setRound(amount){return getStateNode().setState({round: amount})}
+function setHealth(amount){return getStateNode().setState({health: amount})}
