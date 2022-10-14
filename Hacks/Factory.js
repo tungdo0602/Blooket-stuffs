20eedbc43d254890529d6b4951c341ab45be06dd
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 18:50:52 +0700

    Update Factory.js

--- Hacks/Factory.js
+++ Hacks/Factory.js
@@ -1,11 +1,11 @@
-function addCash(amount){return getStateNode()[0]._owner.stateNode.setState({cash: amount})}
+function addCash(amount){return getStateNode().setState({cash: amount})}
 function freeUpgrade(){
-    for (let i of getStateNode()[0]._owner.stateNode.state.blooks){
+    for (let i of getStateNode().state.blooks){
         for(let p=0;p<i.price.length;p++){
             i.price[p]=0;
         }
     }
 }
-function getModMaxBlook(){
-    for(let i=0;i<8;i++){ getStateNode()[0]._owner.stateNode.state.blooks[i] = { "name": "Mega Bot", "color": "#d71f27", "class": "🤖", "rarity": "Legendary", "cash": [ 1e100, 1e100, 1e100, 1e100, 1e100 ], "time": [ 0.1, 0.1, 0.1, 0.1, 0.1 ], "price": [ 0, 0, 0, 0 ], "active": false, "level": 4, "bonus": 1.5 }; }
+function getGodBlooks(){
+    for(let i=0;i<8;i++){ getStateNode().state.blooks[i] = { "name": "Mega Bot", "color": "#d71f27", "class": "🤖", "rarity": "Legendary", "cash": [ 1e100, 1e100, 1e100, 1e100, 1e100 ], "time": [ 0.1, 0.1, 0.1, 0.1, 0.1 ], "price": [ 0, 0, 0, 0 ], "active": false, "level": 4, "bonus": 1.5 }; }
 }
