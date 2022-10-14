23602b9c2a27085c3c32b66a10e718feef58ab83
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 20:10:22 +0700

    Create TowerOfDoom.js

---
+++ Hacks/TowerOfDoom.js
@@ -0,0 +1,8 @@
+function setCoins(amount){return getStateNode().setState({coins: amount})}
+function maxCardStats(){
+    let myCards = getStateNode().state.myCards
+    for(let i of Object.values(myCards)){
+        i.strength = i.charisma = i.wisdom = Number.MAX_VALUE;
+    }
+    return getStateNode().setState({myCards: myCards})
+}
