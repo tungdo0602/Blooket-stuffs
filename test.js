698fcede749ef1698ac20953143c38e31e2c98a9
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-14 19:39:36 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-for(let i=0;i<8;i++){ getStateNode()[0]._owner.stateNode.state.blooks[i] = { "name": "Mega Bot", "color": "#d71f27", "class": "🤖", "rarity": "Legendary", "cash": [ 80000, 430000, 4200000, 62000000, 1000000000 ], "time": [ 1, 1, 1, 1, 1 ], "price": [ 0, 0, 0, 0 ], "active": false, "level": 4, "bonus": 1.5 }; }
+//insert test code here
