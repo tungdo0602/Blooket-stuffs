3684bbee9c6bacbb3477b0085ee9c08832bcac8f
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-15 08:34:14 +0700

    Update AllAnswersCorrect.js

--- Hacks/AllAnswersCorrect.js
+++ Hacks/AllAnswersCorrect.js
@@ -1,7 +1,18 @@
-window.setInterval(()=>{
+(()=>{
+    function getStateNode(){
+    for(let i of Object.keys(document.querySelector("#app>div>div"))){
+        if(i.toString().includes("__reactEventHandlers")){
+            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+            }
+        }
+    }
+}
+    window.setInterval(()=>{
     try{
         getStateNode().state.question.correctAnswers = getStateNode().state.question.answers
     }catch {
         
     }
-},10)
+},10);
+})();
