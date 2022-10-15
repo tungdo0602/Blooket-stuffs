a115cd8ebd248c74c5f41bacf4d7c8371d5d5b75
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-15 08:33:15 +0700

    Update statsMax.js

--- Client Side/statsMax.js
+++ Client Side/statsMax.js
@@ -1,2 +1,19 @@
 //https://dashboard.blooket.com/stats
-getStateNode().setState({level: 1000, xp: 40})
+(()=>{
+    function getStateNode(){
+    for(let i of Object.keys(document.querySelector("#app>div>div"))){
+        if(i.toString().includes("__reactEventHandlers")){
+            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+            }
+        }
+    }
+};
+    function bypass(){
+    let _bypass = document.createElement("iframe");
+    _bypass.style.display = 'none';
+    document.body.appendChild(_bypass);
+    return {window: _bypass.contentWindow, document: _bypass.contentDocument}
+};
+    getStateNode().setState({level: 1000, xp: 40});
+})();
