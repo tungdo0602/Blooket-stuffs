c256bcafe1ed5ab68e44d68a4501f597d3b82f64
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-18 22:08:22 +0700

    Update unlockAllBlooksGameLobby.js

--- Hacks/unlockAllBlooksGameLobby.js
+++ Hacks/unlockAllBlooksGameLobby.js
@@ -1,1 +1,12 @@
-getStateNode().setState({lockedBlooks: [], takenBlooks: []});
+(function(){
+function getStateNode(){
+    for(let i of Object.keys(document.querySelector("#app>div>div"))){
+        if(i.toString().includes("__reactEventHandlers")){
+            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+            }
+        }
+    }
+}
+getStateNode().state.unlocks = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find(x=>x.exports?.a?.toString().includes('UFO')).exports.a.toString().split('"').filter(b => !(b.startsWith(';') || b.startsWith(':') || b.startsWith('function') || b.startsWith('}')))
+})();
