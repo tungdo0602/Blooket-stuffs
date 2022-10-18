fd732d2403124156916cd8d427ca73eb3fea5b8d
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-18 20:22:54 +0700

    Create addWeight.js

---
+++ Hacks/Fishing Frenzy/addWeight.js
@@ -0,0 +1,19 @@
+(function(){
+function getStateNode(){
+    for(let i of Object.keys(document.querySelector("#app>div>div"))){
+        if(i.toString().includes("__reactEventHandlers")){
+            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+            }
+        }
+    }
+}
+function bypass(){
+    let _bypass = document.createElement("iframe");
+    _bypass.style.display = 'none';
+    document.body.appendChild(_bypass);
+    return {window: _bypass.contentWindow, document: _bypass.contentDocument}
+}
+w = Number(bypass().window.prompt("Lbs?"))
+getStateNode().setState({weight: w, weight2: w})
+})();
