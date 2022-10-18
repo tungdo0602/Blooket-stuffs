7ef655274a55abdb12c1e6c61be573fdacae9591
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-18 20:35:25 +0700

    Create autoClickAnswer.js

---
+++ Hacks/Battle Royale/autoClickAnswer.js
@@ -0,0 +1,18 @@
+(function(){
+function getStateNode(){
+    for(let i of Object.keys(document.querySelector("#app>div>div"))){
+        if(i.toString().includes("__reactEventHandlers")){
+            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+            }
+        }
+    }
+}
+window.setInterval(()=>{
+    try{
+        document.querySelector("#answer"+getStateNode().props.client.question.answers.indexOf(getStateNode().props.client.question.correctAnswers[0].toString())+">div>div>div").click()
+    }catch {
+        
+    }
+},10);
+})();
