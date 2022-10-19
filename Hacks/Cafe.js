4e9f16611e12d4c4d4500200aaf24d172beb99c3
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-19 23:31:33 +0700

    Delete Cafe.js

--- Hacks/Cafe.js
+++
@@ -1,8 +0,0 @@
-function setCash(amount){return getStateNode().setState({cafeCash: amount})}
-function maxFood(){
-for(let i of Object.values(getStateNode().state.foods)){
-    i.level = 5
-    i.stock = Number.MAX_VALUE
-}
-}
-function setDay(amount){return getStateNode().setState({day: amount})}
