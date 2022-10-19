7926ded50af9f5628a3956b0aec5e427f3bab642
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-19 23:31:45 +0700

    Delete CrazyKingdom.js

--- Hacks/CrazyKingdom.js
+++
@@ -1,4 +0,0 @@
-function maxAll(){return getStateNode().setState({materials: Number.MAX_VALUE, people: Number.MAX_VALUE, happiness: Number.MAX_VALUE, gold: Number.MAX_VALUE})}
-function setScore(amount){
-    return getStateNode().setState({guestScore: amount})
-}
