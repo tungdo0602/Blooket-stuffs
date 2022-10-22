9d9193fd9f8dd6dfc1da72120b4ba1366e81510c
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-10-22 19:40:10 +0700

    Update blooketLobbyUtilities.user.js

--- userscripts/blooketLobbyUtilities.user.js
+++ userscripts/blooketLobbyUtilities.user.js
@@ -18,22 +18,22 @@
     'use strict';
 (function(){
     function getStateNode(){
-    for(let i of Object.keys(document.querySelector("#app>div>div"))){
-        if(i.toString().includes("__reactEventHandlers")){
-            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
-                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+        for(let i of Object.keys(document.querySelector("#app>div>div"))){
+            if(i.toString().includes("__reactEventHandlers")){
+                for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
+                    if(p._owner&&p._owner.stateNode)return p._owner.stateNode
+                }
             }
         }
     }
-}
-function bypass(){
-    let _bypass = document.createElement("iframe");
-    _bypass.style.display = 'none';
-    document.body.appendChild(_bypass);
-    return {window: _bypass.contentWindow, document: _bypass.contentDocument}
-}
-function findByProp(prop,first=true){
-    const wp = Object.values(webpackJsonp.push([
+    function bypass(){
+        let _bypass = document.createElement("iframe");
+        _bypass.style.display = 'none';
+        document.body.appendChild(_bypass);
+        return {window: _bypass.contentWindow, document: _bypass.contentDocument}
+    }
+    function findByProp(prop,first=true){
+        const wp = Object.values(webpackJsonp.push([
             [], {
                 ['']: (_, a, b) => {
                     a.cache = b.c
@@ -43,11 +43,11 @@ function findByProp(prop,first=true){
                 ['']
             ],
         ]).cache);
-    if(first){
-        return wp.find((x)=>x.exports&&x.exports.a&&x.exports.a[prop]).exports.a;
-    }else{
-        return wp.filter(x=>x.exports?.a&&x.exports?.a[prop]).map(n=>n.exports.a)
-    }
+        if(first){
+            return wp.find((x)=>x.exports&&x.exports.a&&x.exports.a[prop]).exports.a;
+        }else{
+            return wp.filter(x=>x.exports?.a&&x.exports?.a[prop]).map(n=>n.exports.a)
+        }
     }
     function checkTomorrow(){
         let tmr = new Date();
