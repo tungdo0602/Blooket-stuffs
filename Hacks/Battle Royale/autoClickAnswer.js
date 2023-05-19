47b953d70391c357e210d72101d76c3bf0e3e963
Tung <89498812+tungdo0602@users.noreply.github.com>
2023-05-19 23:22:37 +0700

    Update autoClickAnswer.js

--- Hacks/Battle Royale/autoClickAnswer.js
+++ Hacks/Battle Royale/autoClickAnswer.js
@@ -10,7 +10,7 @@ function getStateNode(){
 }
 window.setInterval(()=>{
     try{
-        getStateNode().onAnswer(getStateNode().props.client.question.correctAnswers[0].toString());
+        document.querySelector("#answer"+getStateNode().props.client.question.answers.indexOf(getStateNode().props.client.question.correctAnswers[0])).click()
     }catch {
         
     }
