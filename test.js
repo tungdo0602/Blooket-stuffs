63db95535887826777f057923c0619d0a1e4c242
Tung <89498812+tungdo0602@users.noreply.github.com>
2023-12-23 19:18:56 +0700

    in development

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-//insert test code here
+function findByProp(prop,first=true){
+const wp = Object.values(webpackJsonp.push([
+        [], {
+            ['']: (_, a, b) => {
+                a.cache = b.c
+            },
+        },
+        [
+            ['']
+        ],
+    ]).cache);
+if(first){
+    return wp.find((x)=>x.exports&&x.exports.a&&x.exports.a[prop]).exports.a;
+}else{
+    return wp.filter(x=>x.exports?.a&&x.exports?.a[prop]).map(n=>n.exports.a)
+}
+}
