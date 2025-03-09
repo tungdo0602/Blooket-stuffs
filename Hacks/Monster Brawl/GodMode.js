a47d704b1da32c12d0c4c8ddf9ed4ab7789a3859
Tung <89498812+tungdo0602@users.noreply.github.com>
2025-03-09 12:53:56 +0700

    Update GodMode.js

--- Hacks/Monster Brawl/GodMode.js
+++ Hacks/Monster Brawl/GodMode.js
@@ -1,12 +1,12 @@
 (function(){
-    function getStateNode(){
-    for(let i of Object.keys(document.querySelector("#app>div>div"))){
-        if(i.toString().includes("__reactEventHandlers")){
-            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
-                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
-            }
-        }
-    }
+function getStateNode() {
+    return Object.values(document.querySelector("#app>div>div")).find(e=>e.children).children.find(m=>m._owner&&m._owner.stateNode)._owner.stateNode;
+}
+function bypass(){
+    let _bypass = document.createElement("iframe");
+    _bypass.style.display = 'none';
+    document.body.appendChild(_bypass);
+    return {window: _bypass.contentWindow, document: _bypass.contentDocument}
 }
     getStateNode().state.game.instance.events._events['game-over'].map(a=>a.fn=()=>{});
 })();
