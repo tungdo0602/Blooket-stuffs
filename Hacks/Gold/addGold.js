c8c3e0b5f8b0c808c1428a66c01de9623c6e1380
Tung <89498812+tungdo0602@users.noreply.github.com>
2025-03-09 12:09:03 +0700

    Update addGold.js

--- Hacks/Gold/addGold.js
+++ Hacks/Gold/addGold.js
@@ -1,12 +1,6 @@
 (function(){
-function getStateNode(){
-    for(let i of Object.keys(document.querySelector("#app>div>div"))){
-        if(i.toString().includes("__reactEventHandlers")){
-            for(let p of Object.values(document.querySelector("#app>div>div")[i].children.filter(n=>n))){
-                if(p._owner&&p._owner.stateNode)return p._owner.stateNode
-            }
-        }
-    }
+function getStateNode() {
+    return Object.values(document.querySelector("#app>div>div")).find(e=>e.children).children.find(m=>m._owner&&m._owner.stateNode)._owner.stateNode;
 }
 function bypass(){
     let _bypass = document.createElement("iframe");
@@ -14,19 +8,6 @@ function bypass(){
     document.body.appendChild(_bypass);
     return {window: _bypass.contentWindow, document: _bypass.contentDocument}
 }
-function findByProp(p){
-return Object.values(webpackJsonp.push([
-    [], {
-        ['']: (_, a, b) => {
-            a.cache = b.c
-        },
-    },
-    [
-        ['']
-    ],
-]).cache).find((x)=>{
-    if(x.exports&&x.exports.a&&x.exports.a[p])return x.exports.a
-}).exports.a}
     let gold = Number(bypass().window.prompt("Gold?"))
     getStateNode().setState({gold: gold, gold2: gold})
 })();
