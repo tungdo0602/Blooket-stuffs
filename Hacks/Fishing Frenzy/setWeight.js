3696b577a441803d8775c31767561ba092af8604
Tung <89498812+tungdo0602@users.noreply.github.com>
2025-03-09 12:36:10 +0700

    Update and rename addWeight.js to setWeight.js

---
+++ Hacks/Fishing Frenzy/setWeight.js
@@ -0,0 +1,13 @@
+(function(){
+function getStateNode() {
+    return Object.values(document.querySelector("#app>div>div")).find(e=>e.children).children.find(m=>m._owner&&m._owner.stateNode)._owner.stateNode;
+}
+function bypass(){
+    let _bypass = document.createElement("iframe");
+    _bypass.style.display = 'none';
+    document.body.appendChild(_bypass);
+    return {window: _bypass.contentWindow, document: _bypass.contentDocument}
+}
+w = Number(bypass().window.prompt("Lbs?"))
+getStateNode().setState({weight: w, weight2: w})
+})();
